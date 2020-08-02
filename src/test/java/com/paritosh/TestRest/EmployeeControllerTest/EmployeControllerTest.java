933ea1f7544a703f9bfef32f061bd6daa014ed1a
Paritoshgk <kulkarni.pari123@gmail.com>
2020-08-02 09:04:43 +0530

    Added Latest code

---
+++ src/test/java/com/paritosh/TestRest/EmployeeControllerTest/EmployeControllerTest.java
@@ -0,0 +1,11 @@
+package com.paritosh.TestRest.EmployeeControllerTest;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class EmployeControllerTest 
+{
+	
+   
+}
